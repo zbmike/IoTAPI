5fa266325adccc6bbc38b10dab260deedef8106b
Zhiyuan He <zh2312@columbia.edu>
2021-06-15 22:29:42 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :devices, only: [:show, :create] do
+    resources :data, only: [:create]
+  end
 end
